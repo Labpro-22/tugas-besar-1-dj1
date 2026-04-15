733028918a370aa749c76a19f75c7c588f6e7b39
Faizalfadaa <faizalfadha@gmail.com>
2026-04-15 17:42:00 +0700

    fix: fix cardDeck implementation

--- src/models/Board/CardDeck.cpp
+++ src/models/Board/CardDeck.cpp
@@ -14,32 +14,38 @@ CardDeck<T>::~CardDeck() {
 template<class T>
 void CardDeck<T>::shuffle() {
     random_device random;
-    mt19937 g(random);
+    mt19937 g(random());
 
-    shuffle(drawPile.begin(), drawPile.end(), g);
+    std::shuffle(drawPile.begin(), drawPile.end(), g);
 }
 
 template<class T>
 void CardDeck<T>::initialize(vector<T> card) {
     for(auto it = card.begin(); it < card.end(); ++it) {
-        drawPile.push_back(it);
+        drawPile.push_back(*it);
     }
+
+    discardPile.clear();
 }
 
 template<class T>
 T CardDeck<T>::draw() {
-    return drawPile.back();
+    if(!drawPile.empty()) {
+        T drawCard = drawPile.back();
+        drawPile.pop_back();
+        return drawCard;
+    }
 }
 
 template<class T>
 void CardDeck<T>::discard(T card) {
-    for(auto it = drawPile.end(); it != drawPile.begin(); it--) {
-        if(it == card) {
-            drawPile.erase(it);
-            discardPile.push_back(it);
-            break;
-        }
+    reverse(drawPile.begin(), drawPile.end());
+    auto it = find(drawPile.begin(), drawPile.end(), card);
+    if(it != drawPile.end()) {
+        discardPile.push_back(*it);
+        drawPile.erase(it);
     }
+    reverse(drawPile.begin(), drawPile.end());
 }
 
 template<class T>
@@ -56,7 +62,8 @@ template<class T>
 vector<T> CardDeck<T>::getAllCards() {
     vector<T> deck;
     for(auto it = drawPile.begin(); it < drawPile.end(); ++it) {
-        deck.push_back(it);
+        deck.push_back(*it);
     }
+    return deck;
 }
 
